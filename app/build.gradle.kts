365c54864d65dd0a1f471232813460b593e37123
Jamie Baddeley <jamie@mixmat.es>
2026-04-17 14:56:19 +1200

    Bump version to 0.3.0 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "es.mixmat.listener"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
